37438c39b5140df9b3e80c2e98cd5abb80a2cf41
bonan <zbn.dale@gmail.com>
2023-08-09 17:24:51 -0400

    Import existing experiment

--- static/js/task.js
+++ static/js/task.js
@@ -1,227 +1,172 @@
-/*
- * Requires:
- *     psiturk.js
- *     utils.js
- */
 
-// Initalize psiturk object
-var psiTurk = new PsiTurk(uniqueId, adServerLoc, mode);
+/* Data */
+let start_task_time = 0;
+let subjectData = {};
 
-var mycondition = condition;  // these two variables are passed by the psiturk server process
-var mycounterbalance = counterbalance;  // they tell you which condition you have been assigned to
-// they are not used in the stroop code but may be useful to you
 
-// All pages to be loaded
-var pages = [
-	"instructions/instruct-1.html",
-	"instructions/instruct-2.html",
-	"instructions/instruct-3.html",
-	"instructions/instruct-ready.html",
-	"stage.html",
-	"postquestionnaire.html"
+/* Assign task items */
+const N_TASK = 20;
+const ALL_ITEMS = [
+  'soil', 'rabbit-caught', 'berry-harvest', 'stone', 'rabbit', 'bow', 'rabbit-housed',
+  'water', 'branch', 'seedling', 'mushroom', 'steel', 'arrow', 'berry', 'sheep',
 ];
+const ALL_CELL_IDS = getAllCellIds();
 
-psiTurk.preloadPages(pages);
+let [ clickData, clickDataKeys ] =[ {}, [] ];
+let [ task_items, task_cells, task_cell_item] = [ {}, {}, {} ];
 
-var instructionPages = [ // add as a list as many pages as you like
-	"instructions/instruct-1.html",
-	"instructions/instruct-2.html",
-	"instructions/instruct-3.html",
-	"instructions/instruct-ready.html"
-];
+for (let i = 0; i < N_TASK; i++) {
+  let task_size = randFromRange(2, 9);
+  task_items[`task_${i+1}`] = sampleFromList(ALL_ITEMS, n=task_size); // sample items
+  task_cells[`task_${i+1}`] = sampleFromList(ALL_CELL_IDS, n=task_size, replace=false); // sample cell-ids
 
+  let fullCellIds = task_cells[`task_${i+1}`].map(el => `task${i+1}-grid-${el}`); // get full-name cell-ids
+  fullCellIds.forEach((el, idx) => task_cell_item[el] = task_items[`task_${i+1}`][idx]); // append items
 
-/********************
-* HTML manipulation
-*
-* All HTML files in the templates directory are requested 
-* from the server when the PsiTurk object is created above. We
-* need code to get those pages from the PsiTurk object and 
-* insert them into the document.
-*
-********************/
-
-/********************
-* STROOP TEST       *
-********************/
-var StroopExperiment = function() {
-
-	var wordon, // time word is presented
-	    listening = false;
-
-	// Stimuli for a basic Stroop experiment
-	var stims = [
-			["SHIP", "red", "unrelated"],
-			["MONKEY", "green", "unrelated"],
-			["ZAMBONI", "blue", "unrelated"],
-			["RED", "red", "congruent"],
-			["GREEN", "green", "congruent"],
-			["BLUE", "blue", "congruent"],
-			["GREEN", "red", "incongruent"],
-			["BLUE", "green", "incongruent"],
-			["RED", "blue", "incongruent"]
-		];
-
-	stims = _.shuffle(stims);
-
-	var next = function() {
-		if (stims.length===0) {
-			finish();
-		}
-		else {
-			stim = stims.shift();
-			show_word( stim[0], stim[1] );
-			wordon = new Date().getTime();
-			listening = true;
-			d3.select("#query").html('<p id="prompt">Type "R" for Red, "B" for blue, "G" for green.</p>');
-		}
-	};
-	
-	var response_handler = function(e) {
-		if (!listening) return;
-
-		var keyCode = e.keyCode,
-			response;
-
-		switch (keyCode) {
-			case 82:
-				// "R"
-				response="red";
-				break;
-			case 71:
-				// "G"
-				response="green";
-				break;
-			case 66:
-				// "B"
-				response="blue";
-				break;
-			default:
-				response = "";
-				break;
-		}
-		if (response.length>0) {
-			listening = false;
-			var hit = response == stim[1];
-			var rt = new Date().getTime() - wordon;
-
-			psiTurk.recordTrialData({'phase':"TEST",
-                                     'word':stim[0],
-                                     'color':stim[1],
-                                     'relation':stim[2],
-                                     'response':response,
-                                     'hit':hit,
-                                     'rt':rt}
-                                   );
-			remove_word();
-			next();
-		}
-	};
-
-	var finish = function() {
-	    $("body").unbind("keydown", response_handler); // Unbind keys
-	    currentview = new Questionnaire();
-	};
-	
-	var show_word = function(text, color) {
-		d3.select("#stim")
-			.append("div")
-			.attr("id","word")
-			.style("color",color)
-			.style("text-align","center")
-			.style("font-size","150px")
-			.style("font-weight","400")
-			.style("margin","20px")
-			.text(text);
-	};
-
-	var remove_word = function() {
-		d3.select("#word").remove();
-	};
-
-	
-	// Load the stage.html snippet into the body of the page
-	psiTurk.showPage('stage.html');
-
-	// Register the response handler that is defined above to handle any
-	// key down events.
-	$("body").focus().keydown(response_handler); 
-
-	// Start the test
-	next();
-};
+  clickDataKeys = clickDataKeys.concat(fullCellIds);
+}
+clickDataKeys.forEach(el => clickData[el] = 0);
 
 
-/****************
-* Questionnaire *
-****************/
-
-var Questionnaire = function() {
-
-	var error_message = "<h1>Oops!</h1><p>Something went wrong submitting your HIT. This might happen if you lose your internet connection. Press the button to resubmit.</p><button id='resubmit'>Resubmit</button>";
-
-	record_responses = function() {
-
-		psiTurk.recordTrialData({'phase':'postquestionnaire', 'status':'submit'});
-
-		$('textarea').each( function(i, val) {
-			psiTurk.recordUnstructuredData(this.id, this.value);
-		});
-		$('select').each( function(i, val) {
-			psiTurk.recordUnstructuredData(this.id, this.value);		
-		});
-
-	};
-
-	prompt_resubmit = function() {
-		document.body.innerHTML = error_message;
-		$("#resubmit").click(resubmit);
-	};
-
-	resubmit = function() {
-		document.body.innerHTML = "<h1>Trying to resubmit...</h1>";
-		reprompt = setTimeout(prompt_resubmit, 10000);
-		
-		psiTurk.saveData({
-			success: function() {
-			    clearInterval(reprompt); 
-                psiTurk.computeBonus('compute_bonus', function(){
-                	psiTurk.completeHIT(); // when finished saving compute bonus, the quit
-                }); 
 
 
-			}, 
-			error: prompt_resubmit
-		});
-	};
 
-	// Load the questionnaire snippet 
-	psiTurk.showPage('postquestionnaire.html');
-	psiTurk.recordTrialData({'phase':'postquestionnaire', 'status':'begin'});
-	
-	$("#next").click(function () {
-	    record_responses();
-	    psiTurk.saveData({
-            success: function(){
-                psiTurk.computeBonus('compute_bonus', function() { 
-                	psiTurk.completeHIT(); // when finished saving compute bonus, the quit
-                }); 
-            }, 
-            error: prompt_resubmit});
-	});
-    
-	
-};
+/* Collect prolific id */
+function handle_prolific() {
+  subjectData['prolific_id'] = getEl('prolific_id_text').value;
+  hideAndShowNext('prolific_id', 'instruction', 'block');
+}
 
-// Task object to keep track of the current phase
-var currentview;
 
-/*******************
- * Run Task
- ******************/
-$(window).load( function(){
-    psiTurk.doInstructions(
-    	instructionPages, // a list of pages you want to display in sequence
-    	function() { currentview = new StroopExperiment(); } // what you want to do when you are done with instructions
-    );
-});
+/* Create task div */
+for (let tid = 1; tid <= N_TASK; tid++) {
+
+  let taskDiv = createCustomElement('div', '', `task-${tid}`);
+
+  // Progress bar
+  let progressDiv = createCustomElement('div', 'progress-div', `progress-div-${tid}`);
+  progressDiv.innerHTML = `<label for="progress-bar">Progress:</label><progress id="progress-bar-${tid}" value="${(tid+1)/(N_TASK+1)*100}" max="100"></progress>`
+
+  // Main task box
+  let mainBoxDiv = createCustomElement('div', 'main-box', `main-box-${tid}`);
+  let itemsBox= createCustomElement('div', 'items-box', `items-box-${tid}`);
+  let itemsTab = createCustomElement('table', 'worktop-table', id=`items-tab-${tid}`);
+
+  // Draw grid
+  for (let i = 0; i < NROW; i++) {
+    let wtrows = itemsTab.insertRow();
+    for (let j = 0; j < NCOL; j++) {
+      let tcell = wtrows.insertCell();
+
+      let tcellId = `task${tid}-grid-` + (j+1).toString() + '-' + (NROW-i).toString();
+      tcell.id = tcellId;
+
+      //tcell.style.border = 'red solid 1px';
+      tcell.style.width = '40px';
+
+      if (Object.keys(task_cell_item).indexOf(tcellId) > -1 ) {
+        tcell.innerHTML = drawItem(task_cell_item[tcellId]);
+        tcell.onclick = () => cellClick(tcellId);
+      }
+
+    }
+  }
+  itemsBox.append(itemsTab);
+  mainBoxDiv.append(itemsBox);
+
+  // Task button
+  let buttonDiv = createCustomElement('div', 'button-group-vc', '');
+  let taskBtn = createBtn(`task-confirm-${tid}`, 'Confirm', true, 'big-button');
+  taskBtn.onclick = () => task_next(tid);
+  buttonDiv.append(taskBtn)
+
+  // Assemble
+  taskDiv.append(progressDiv);
+  taskDiv.append(mainBoxDiv);
+  taskDiv.append(buttonDiv);
+
+  getEl('task').append(taskDiv);
+  taskDiv.style.display = (tid==1)? 'block': 'none';
+
+}
+function task_next(id) {
+  if (id < N_TASK) {
+    hideAndShowNext(`task-${id}`, `task-${id+1}`, "block");
+  } else {
+    hideAndShowNext("task", "debrief", "block");
+  }
+}
+function cellClick (cell_id) {
+  clickData[cell_id] += 1;
+  if (clickData[cell_id] % 2 == 1) {
+    getEl(cell_id).style.border = 'solid red 2px';
+  } else {
+    getEl(cell_id).style.border = '0px';
+  }
+}
+
+
+/* Comprehension quiz */
+const checks = [ 'check1', 'check2', 'check3', 'check4', 'check5' ];
+const answers = [ false, false, true, false, true ];
+
+function check_quiz() {
+  getEl('check-btn').style.display = 'none';
+
+  let inputs = [];
+  checks.map(check => {
+    const vals = document.getElementsByName(check);
+    inputs.push(vals[0].checked);
+  });
+  const pass = (inputs.join('') === answers.join(''));
+
+  if (pass) {
+    showNext('pass', 'block');
+  } else {
+    showNext('retry', 'block');
+  }
+}
+function handle_pass() {
+  start_task_time = Date.now();
+  hide("pass");
+  hide("quiz");
+  showNext("task", "block");
+}
+function handle_retry() {
+  hide("retry");
+  hide("quiz");
+  showNext("instruction", "block");
+  getEl('check-btn').style.display = 'flex';
+}
+getEl('prequiz').onchange = () => compIsFilled() ? getEl('check-btn').disabled = false : null;
+
+
+
+/* Bebrief */
+getEl('postquiz').onchange = () => isFilled('postquiz')? getEl('done-btn').disabled = false: null;
+
+function is_done(complete_code) {
+  let inputs = getEl('postquiz').elements;
+  Object.keys(inputs).forEach(id => subjectData[inputs[id].name] = inputs[id].value);
+
+  // Clean up free responses
+  subjectData['feedback'] = removeSpecial(subjectData['feedback']);
+
+  const end_time = new Date();
+  let token = generateToken(8);
+
+  // Save data
+  let clientData = {};
+  clientData.subject = subjectData;
+  clientData.subject.date = formatDates(end_time, 'date');
+  clientData.subject.time = formatDates(end_time, 'time');
+  clientData.subject.task_duration = end_time - start_task_time;
+  clientData.subject.token = token;
+
+  // Transit
+  hideAndShowNext("debrief", "completed", 'block');
+  getEl('completion-code').append(document.createTextNode(complete_code));
+
+  // download(JSON.stringify(clientData), 'data.txt', '"text/csv"');
+  console.log(clientData);
+}
---
+++ static/js/utilities.js
@@ -0,0 +1,213 @@
+
+const NCOL = 6
+const NROW = 10
+
+/* Custom wrappers */
+function createCustomElement (type = 'div', className, id) {
+  let element = (["svg", "polygon"].indexOf(type) < 0)?
+    document.createElement(type):
+    document.createElementNS("http://www.w3.org/2000/svg", type);
+  if (className.length > 0) element.setAttribute("class", className);
+  if (id.length > 0) element.setAttribute("id", id);
+  return element;
+}
+function createText(h = "h1", text = 'hello') {
+  let element = document.createElement(h);
+  let tx = document.createTextNode(text);
+  element.append(tx);
+  return(element)
+}
+function createBtn (btnId, text = "Button", on = true, className = "task-button") {
+  let btn = createCustomElement("button", className, btnId);
+  btn.disabled = !on;
+  (text.length > 0) ? btn.append(document.createTextNode(text)): null;
+  return(btn)
+}
+function getEl(elementID) {
+  let el = document.getElementById(elementID)
+  return el
+}
+function setAttributes(el, attrs) {
+  for(var key in attrs) {
+    el.setAttribute(key, attrs[key]);
+  }
+}
+
+
+/* Transition functions */
+function showNext(id, display = "flex", center = true) {
+  let div = document.getElementById(id);
+  div.style.display = display;
+  div.scrollIntoView(center);
+}
+function hide(id) {
+  let div = document.getElementById(id);
+  div.style.display = "none";
+}
+function hideAndShowNext(hid, sid, display, center = true) {
+  hide(hid);
+  showNext(sid, display, center)
+}
+
+
+
+/* Form-related functions */
+function compIsFilled () {
+  let radios = document.getElementsByTagName('input');
+  let checked = 0;
+  for (let i = 0; i < radios.length; i++) {
+      checked += radios[i].checked;
+  }
+  return (checked > checks.length-1)
+}
+function isFilled (formID) {
+  let notFilled = false;
+  const nulls = [ '', '--', '', '--', '', '--' ];
+  const form = document.getElementById(formID);
+  const inputs = form.elements;
+  (Object.keys(inputs)).forEach((input, idx) => {
+    let field = inputs[input];
+    notFilled = (notFilled || (field.value === nulls[idx]));
+  });
+  return (!notFilled)
+}
+
+
+/* Data-related functions */
+function removeSpecial (text) {
+  text = text.replace(/[&\/\\#,$~%"\[\]{}@^_|`']/gi, '');
+  text = text.replace(/(\r\n|\n|\r|\t)/gm, " ")
+  return text
+}
+function generateToken (length) {
+  let tokens = '';
+  let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  for (let i = 0; i < length; i ++) {
+      tokens += chars.charAt(Math.floor(Math.random() * chars.length))
+  }
+  return tokens;
+}
+function formatDates (date, option = 'date') {
+  let year = date.getFullYear();
+  let month = String(date.getMonth() + 1).padStart(2, '0');
+  let day = String(date.getDate() + 1).padStart(2, '0');
+  let hour = String(date.getHours()+ 1).padStart(2, '0');
+  let min = String(date.getMinutes() + 1).padStart(2, '0');
+  let sec = String(date.getSeconds() + 1).padStart(2, '0');
+  dateParts = (option === 'date') ? [ year, month, day ] : [ hour, min, sec ];
+  return dateParts.join('_');
+}
+function download(content, fileName, contentType) {
+  var a = document.createElement("a");
+  var file = new Blob([content], {type: contentType});
+  a.href = URL.createObjectURL(file);
+  a.download = fileName;
+  a.click();
+}
+
+
+/* Random functions */
+function randFromRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+function sampleFromList(arr, n=1, replace=true) {
+  if (n==1) {
+    return (arr[Math.floor(Math.random()*arr.length)])
+  }
+  else {
+    let sampled = [];
+    for (let j = 0; j < n; j++) {
+      let randomIndex = Math.floor(Math.random()*arr.length);
+      sampled.push(arr[randomIndex]);
+      if (replace == 0) {
+        arr.slice(randomIndex, 1)[0];
+      }
+    }
+    return sampled;
+  }
+}
+
+
+/* Object functions */
+function swapObjectKeyValue(obj){
+  var ret = {};
+  for(var key in obj){
+    ret[obj[key]] = key;
+  }
+  return ret;
+}
+
+
+/* Task-specific functions */
+function drawItem (item) {
+  return (item.length > 0)? `<img src="static/imgs/${item}.png" style="height:60px">`: '';
+}
+function getAllCellIds(ncol=NCOL, nrow=NROW) {
+  let ret = [];
+  for (let i = 0; i < nrow; i++) {
+    for (let j = 0; j < ncol; j++) {
+      ret.push((j+1).toString() + '-' + (NROW-i).toString());
+    }
+  }
+  return ret
+}
+function combineClick() {
+  // Get selected items
+  let selected_items = [];
+  let selected_cells = [];
+  Object.keys(demoState).forEach(key => {
+    if (demoState[key].length > 0 && demoStateCount[key] % 2 == 1) {
+      selected_items.push(demoState[key]);
+      selected_cells.push(key);
+    }
+  });
+  console.log(selected_items)
+
+
+  let combo = selected_items.sort().join('');
+  //console.log(combo)
+
+  // Check if a change should happen
+  if (Object.keys(TECH_TREE).indexOf(combo) > -1 && Math.random() < TECH_TREE[combo][1] ) {
+
+    let items_to_combine = {};
+    selected_cells.forEach(key => items_to_combine[key] = demoState[key]);
+    items_to_combine = swapObjectKeyValue(items_to_combine);
+
+    switch (combo) {
+
+      case 'berrysoilwater':
+        // Make soil => seedling and other two disappear
+        getEl(items_to_combine['soil']).innerHTML = drawItem('seedling');
+        getEl(items_to_combine['soil']).style.border = 'gold solid 10px';
+        getEl(items_to_combine['water']).innerHTML = '';
+        getEl(items_to_combine['water']).style.border = '0px';
+        getEl(items_to_combine['berry']).innerHTML = '';
+        getEl(items_to_combine['berry']).style.border = '0px';
+        selected_items = []; // <-- Not working now
+
+        break;
+
+      case 'branchstone':
+        // Make branch => arrow and stone disappears
+        getEl(items_to_combine['branch']).innerHTML = drawItem('arrow');
+        getEl(items_to_combine['branch']).style.border = 'gold solid 10px';
+        getEl(items_to_combine['stone']).innerHTML = '';
+        getEl(items_to_combine['stone']).style.border = '0px';
+        selected_items = [];
+
+        break;
+
+    }
+
+
+  } else {
+    console.log('Nothing happens')
+  }
+
+
+  // Check if is a reward
+  if (Object.keys(REWARDS).indexOf(combo) > -1) {
+    console.log('Get ' + REWARDS[combo] + 'XP!')
+  }
+}
